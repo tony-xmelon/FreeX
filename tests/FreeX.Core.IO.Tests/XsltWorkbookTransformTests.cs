761a8113d3257d5cb57002eda464c189cc970d7c
tony-xmelon <tony.xmelon@gmail.com>
2026-06-02 03:11:46 +0300

    Add XSLT style AVT parity tests

--- tests/FreeX.Core.IO.Tests/XsltWorkbookTransformTests.cs
+++ tests/FreeX.Core.IO.Tests/XsltWorkbookTransformTests.cs
@@ -1268,6 +1268,62 @@ public sealed class XsltWorkbookTransformTests
     }
 
     [Fact]
+    public void TransformToSpreadsheetXml_StyleAttributeValueTemplates_GenerateSpreadsheetMl()
+    {
+        using var source = StreamFromString("""
+            <report>
+              <style id="money" format="$#,##0.00"/>
+              <style id="percent" format="0.00%"/>
+              <row label="Revenue" amount="42.5" style="money"/>
+              <row label="Margin" amount="0.875" style="percent"/>
+            </report>
+            """);
+        using var stylesheet = StreamFromString("""
+            <xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">
+              <xsl:template match="/report">
+                <ss:Workbook>
+                  <ss:Styles>
+                    <xsl:for-each select="style">
+                      <ss:Style ss:ID="{@id}">
+                        <ss:NumberFormat ss:Format="{@format}" />
+                      </ss:Style>
+                    </xsl:for-each>
+                  </ss:Styles>
+                  <ss:Worksheet ss:Name="Styled AVT">
+                    <ss:Table>
+                      <xsl:for-each select="row">
+                        <ss:Row>
+                          <ss:Cell><ss:Data ss:Type="String"><xsl:value-of select="@label"/></ss:Data></ss:Cell>
+                          <ss:Cell ss:StyleID="{@style}"><ss:Data ss:Type="Number"><xsl:value-of select="@amount"/></ss:Data></ss:Cell>
+                        </ss:Row>
+                      </xsl:for-each>
+                    </ss:Table>
+                  </ss:Worksheet>
+                </ss:Workbook>
+              </xsl:template>
+            </xsl:stylesheet>
+            """);
+
+        using var transformed = XsltWorkbookTransform.TransformToSpreadsheetXml(source, stylesheet);
+
+        XNamespace ss = "urn:schemas-microsoft-com:office:spreadsheet";
+        var document = XDocument.Load(transformed);
+
+        document.Descendants(ss + "Style")
+            .Select(element => element.Attribute(ss + "ID")!.Value)
+            .Should().Equal("money", "percent");
+        document.Descendants(ss + "NumberFormat")
+            .Select(element => element.Attribute(ss + "Format")!.Value)
+            .Should().Equal("$#,##0.00", "0.00%");
+        document.Descendants(ss + "Cell")
+            .Select(element => element.Attribute(ss + "StyleID")?.Value)
+            .Where(value => value is not null)
+            .Should().Equal("money", "percent");
+    }
+
+    [Fact]
     public void TransformToSpreadsheetXml_DecimalFormat_GeneratesFormattedTextCells()
     {
         using var source = StreamFromString("<rows><row amount=\"1234.5\" ratio=\"0.875\" /></rows>");
