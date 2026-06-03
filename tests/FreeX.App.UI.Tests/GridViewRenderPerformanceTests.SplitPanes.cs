0c1e888af94d097c8618a292bdd8a75bd983334b
tony-xmelon <tony.xmelon@gmail.com>
2026-06-04 02:19:24 +0300

    Optimize UI and host performance hot paths

--- tests/FreeX.App.UI.Tests/GridViewRenderPerformanceTests.SplitPanes.cs
+++ tests/FreeX.App.UI.Tests/GridViewRenderPerformanceTests.SplitPanes.cs
@@ -192,10 +192,13 @@ public sealed partial class GridViewRenderPerformanceTests
         buildOccupiedCells.Should().Contain("spansByRow.Add(cell.Row, spans)");
         buildOccupiedCells.Should().Contain("Dictionary<uint, OccupiedColumnSpans>");
         buildOccupiedCells.Should().Contain("AddOccupiedColumn(ref spans, cell.Col, ref needsNormalize)");
-        buildOccupiedCells.Should().Contain("NormalizeOccupiedColumnSpans(spansByRow)");
+        buildOccupiedCells.Should().Contain("HashSet<uint>? rowsNeedingNormalize = null;");
+        buildOccupiedCells.Should().Contain("(rowsNeedingNormalize ??= []).Add(cell.Row);");
+        buildOccupiedCells.Should().Contain("NormalizeOccupiedColumnSpans(spansByRow, rowsNeedingNormalize)");
         buildOccupiedCells.Should().Contain("private struct OccupiedColumnSpans");
         buildOccupiedCells.Should().Contain("private List<OccupiedColumnSpan>? _overflow;");
         buildOccupiedCells.Should().Contain("new SplitPaneOccupiedCellMap(spansByRow)");
+        buildOccupiedCells.Should().NotContain("Keys.ToArray()");
         mergeRangeIndex.Should().Contain("var queryCells = BuildQueryCells(cells);");
         mergeRangeIndex.Should().Contain("mergedRegion.End.Row < queryCells.MinRow");
         mergeRangeIndex.Should().Contain("mergedRegion.Start.Row > queryCells.MaxRow");
