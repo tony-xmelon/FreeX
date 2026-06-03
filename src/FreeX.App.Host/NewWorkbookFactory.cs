2eb3c635a8ce6886004bc8361a5bd9bfa2fa2099
tony-xmelon <tony.xmelon@gmail.com>
2026-06-03 08:47:21 +0300

    Honor Options default font for new workbooks

--- src/FreeX.App.Host/NewWorkbookFactory.cs
+++ src/FreeX.App.Host/NewWorkbookFactory.cs
@@ -4,9 +4,34 @@ namespace FreeX.App.Host;
 
 internal static class NewWorkbookFactory
 {
+    public static Workbook Create(FreeXOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        return Create(
+            options.DefaultSheetCount,
+            options.DefaultFontName,
+            options.DefaultFontSize);
+    }
+
     public static Workbook Create(int defaultSheetCount)
     {
-        var workbook = new Workbook("Book1");
+        return Create(
+            defaultSheetCount,
+            FreeXOptions.DefaultFontNameFallback,
+            FreeXOptions.DefaultFontSizeFallback);
+    }
+
+    private static Workbook Create(
+        int defaultSheetCount,
+        string? defaultFontName,
+        int defaultFontSize)
+    {
+        var defaultStyle = CellStyle.Default.Clone();
+        defaultStyle.FontName = FreeXOptions.NormalizeDefaultFontName(defaultFontName);
+        defaultStyle.FontSize = FreeXOptions.NormalizeDefaultFontSize(defaultFontSize);
+
+        var workbook = new Workbook("Book1", defaultStyle);
         var sheetCount = FreeXOptions.NormalizeDefaultSheetCount(defaultSheetCount);
         for (var sheetIndex = 1; sheetIndex <= sheetCount; sheetIndex++)
             workbook.AddSheet($"Sheet{sheetIndex}");
