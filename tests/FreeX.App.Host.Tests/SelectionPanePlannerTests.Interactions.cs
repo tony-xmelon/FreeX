822f7bae73141bf28a61cb3ba4220e4eb666bf86
tony-xmelon <tony.xmelon@gmail.com>
2026-06-03 17:37:24 +0300

    Split selection pane planner tests

---
+++ tests/FreeX.App.Host.Tests/SelectionPanePlannerTests.Interactions.cs
@@ -0,0 +1,107 @@
+using FluentAssertions;
+using System.IO;
+
+namespace FreeX.App.Host.Tests;
+
+public sealed partial class SelectionPanePlannerTests
+{
+    [Fact]
+    public void SelectionPaneDialogOpenedFromKeyboard_FocusesSearchBox()
+    {
+        var source = ReadSelectionPaneDialogSources();
+
+        source.Should().Contain("Loaded += (_, _) => FocusInitialKeyboardTarget();");
+        source.Should().Contain("private void FocusInitialKeyboardTarget()");
+        source.Should().Contain("DialogFocus.FocusAndSelect(_searchBox);");
+    }
+
+    [Fact]
+    public void SelectionPaneDialog_AllowsInlineRenameInObjectList()
+    {
+        var source = ReadSelectionPaneDialogSources();
+
+        source.Should().Contain("new FrameworkElementFactory(typeof(TextBox))");
+        source.Should().Contain("TextBox.TextProperty");
+        source.Should().Contain("UpdateSourceTrigger = System.Windows.Data.UpdateSourceTrigger.PropertyChanged");
+        source.Should().Contain("ToolTipProperty, UiText.Get(\"SelectionPane_ItemRenameToolTip\")");
+    }
+
+    [Fact]
+    public void SelectionPaneDialog_ListKeyboardShortcutsRenameToggleVisibilityAndReorder()
+    {
+        var source = ReadSelectionPaneDialogSources();
+
+        source.Should().Contain("_list.KeyDown += List_KeyDown;");
+        source.Should().Contain("private void List_KeyDown(object sender, KeyEventArgs e)");
+        source.Should().Contain("TryHandleListReorderShortcut(e)");
+        source.Should().Contain("private bool TryHandleListReorderShortcut(KeyEventArgs e)");
+        source.Should().Contain("ModifierKeys.Control");
+        source.Should().Contain("e.Key == Key.Up");
+        source.Should().Contain("AcceptMove(SelectionPaneDialogAction.MoveUp)");
+        source.Should().Contain("e.Key == Key.Down");
+        source.Should().Contain("AcceptMove(SelectionPaneDialogAction.MoveDown)");
+        source.Should().Contain("if (e.Key == Key.F2)");
+        source.Should().Contain("FocusRenameBox();");
+        source.Should().Contain("if (e.Key == Key.Space)");
+        source.Should().Contain("ToggleSelectedVisibility();");
+        source.Should().Contain("private void FocusRenameBox()");
+        source.Should().Contain("DialogFocus.FocusAndSelect(_renameBox);");
+    }
+
+    [Fact]
+    public void SelectionPaneDialog_AccumulatesMoveChangesInsteadOfClosingOnMove()
+    {
+        var source = ReadSelectionPaneDialogSources();
+        var hostSource = File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "MainWindow.Drawing.cs"));
+
+        source.Should().Contain("private readonly List<SelectionPaneMoveChange> _moveChanges = [];");
+        source.Should().Contain("SelectionPaneDialogStatePlanner.PlanMove");
+        source.Should().Contain("_moveChanges.AddRange(plan.MoveChanges)");
+        source.Should().Contain("ApplySearchAndFilter(selected.Source.Id)");
+        var acceptMoveBody = source.Substring(
+            source.IndexOf("private void AcceptMove", StringComparison.Ordinal),
+            source.IndexOf("private IReadOnlyList<SelectionPaneVisibilityChange>", StringComparison.Ordinal) -
+            source.IndexOf("private void AcceptMove", StringComparison.Ordinal));
+        acceptMoveBody.Should().NotContain("DialogResult = true");
+        hostSource.Should().Contain("SelectionPaneGroupedCommandPlanner.CreateCommand");
+        hostSource.Should().NotContain("SelectionPaneDialogAction.MoveUp when dialog.Result.Target");
+    }
+
+    [Fact]
+    public void SelectionPaneDialog_SupportsDragDropReorder()
+    {
+        var source = ReadSelectionPaneDialogSources();
+
+        source.Should().Contain("_list.AllowDrop = true");
+        source.Should().Contain("_list.PreviewMouseLeftButtonDown");
+        source.Should().Contain("_list.MouseMove");
+        source.Should().Contain("_list.DragOver");
+        source.Should().Contain("_list.Drop");
+        source.Should().Contain("DragDrop.DoDragDrop");
+        source.Should().Contain("SelectionPaneDialogStatePlanner.PlanDragReorder");
+        source.Should().Contain("GetDropPlacement");
+        source.Should().Contain("SelectionPaneDropPlacement.After");
+        source.Should().Contain("CreateDragMoveChanges");
+        source.Should().Contain("PlanDropVisual");
+        source.Should().Contain("IsDropBefore");
+        source.Should().Contain("IsDropAfter");
+        source.Should().Contain("List_DragLeave");
+        source.Should().Contain("ClearDropVisual");
+    }
+
+    [Fact]
+    public void SelectionPaneDialog_MouseMoveClearsStaleDragStateWhenButtonReleased()
+    {
+        var source = ReadSelectionPaneDialogSources();
+        var mouseMove = source[
+            source.IndexOf("private void List_MouseMove", StringComparison.Ordinal)..
+            source.IndexOf("private void List_DragOver", StringComparison.Ordinal)];
+
+        mouseMove.Should().Contain("if (e.LeftButton != System.Windows.Input.MouseButtonState.Pressed)");
+        mouseMove.Should().Contain("_dragStartPoint = null;");
+        mouseMove.Should().Contain("_dragItem = null;");
+        mouseMove.IndexOf("_dragStartPoint = null;", StringComparison.Ordinal)
+            .Should()
+            .BeLessThan(mouseMove.IndexOf("if (_dragStartPoint is not { } start", StringComparison.Ordinal));
+    }
+}
