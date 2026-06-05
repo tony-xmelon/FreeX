2cc378b68a0c6b33213aa0775d24272e6cec7d7d
tony-xmelon <tony.xmelon@gmail.com>
2026-06-05 15:36:25 +0300

    Use shared XAML helpers in command source tests

--- tests/FreeX.App.Host.Tests/HomeFormatAsTableCommandSourceTests.cs
+++ tests/FreeX.App.Host.Tests/HomeFormatAsTableCommandSourceTests.cs
@@ -8,8 +8,8 @@ public sealed class HomeFormatAsTableCommandSourceTests
     [Fact]
     public void FormatAsTableRibbonButton_ExposesExpectedKeyTipAndGalleryMenu()
     {
-        var xaml = File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "MainWindow.xaml"));
-        var button = ExtractButtonElementByClickHandler(xaml, "FormatTableBtn_Click");
+        var xaml = LocalizedXamlTestSupport.ReadMainWindowXaml();
+        var button = xaml.ExtractButtonElementByClickHandler("FormatTableBtn_Click");
 
         button.ShouldContainInvariantCommandName("Format as Table");
         button.Should().Contain("local:RibbonTooltip.KeyTip=\"T\"");
@@ -33,18 +33,5 @@ public sealed class HomeFormatAsTableCommandSourceTests
         source.Should().Contain("new CreateStyledStructuredTableCommand(");
         source.Should().Contain("GroupedSheetRangePlanner.RemapRangeToSheet(dialog.Result.Range, sheetId)");
         source.Should().Contain("tableStyle.Banding");
-    }
-
-    private static string ExtractButtonElementByClickHandler(string xaml, string clickHandler)
-    {
-        var clickIndex = xaml.IndexOf($"Click=\"{clickHandler}\"", StringComparison.Ordinal);
-        clickIndex.Should().BeGreaterThanOrEqualTo(0, $"the {clickHandler} button should be present");
-
-        var start = xaml.LastIndexOf("<Button", clickIndex, StringComparison.Ordinal);
-        start.Should().BeGreaterThanOrEqualTo(0, $"the {clickHandler} button should have a Button start tag");
-
-        var end = xaml.IndexOf("</Button>", clickIndex, StringComparison.Ordinal);
-        end.Should().BeGreaterThanOrEqualTo(clickIndex, $"the {clickHandler} button should have an end tag");
-        return xaml.Substring(start, end - start + "</Button>".Length);
     }
 }
