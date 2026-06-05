4fbc2b3277964c730b7d642069f009ff9aa55e71
tony-xmelon <tony.xmelon@gmail.com>
2026-06-05 17:22:10 +0300

    Use dialog source helper for text to columns tests

--- tests/FreeX.App.Host.Tests/DataToolDialogTests.TextToColumns.cs
+++ tests/FreeX.App.Host.Tests/DataToolDialogTests.TextToColumns.cs
@@ -1,16 +1,13 @@
-using System.IO;
-
 namespace FreeX.App.Host.Tests;
 
 public sealed partial class DataToolDialogTests
 {
     private static string ReadTextToColumnsDialogSources() =>
-        string.Join(
-            Environment.NewLine,
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsDialog.cs")),
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsDialog.FixedWidth.cs")),
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsDialog.ColumnFormats.cs")),
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsDialog.Delimiters.cs")),
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsDialog.Wizard.cs")),
-            File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "TextToColumnsWizardPlanner.cs")));
+        DialogSourceTestSupport.ReadHostSources(
+            "TextToColumnsDialog.cs",
+            "TextToColumnsDialog.FixedWidth.cs",
+            "TextToColumnsDialog.ColumnFormats.cs",
+            "TextToColumnsDialog.Delimiters.cs",
+            "TextToColumnsDialog.Wizard.cs",
+            "TextToColumnsWizardPlanner.cs");
 }
