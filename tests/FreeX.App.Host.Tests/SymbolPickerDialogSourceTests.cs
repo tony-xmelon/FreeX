78abe6491c5e43aa3884baff22e965142754550c
tony-xmelon <tony.xmelon@gmail.com>
2026-06-05 09:34:15 +0300

    Consolidate App.Host logical tree test helpers

--- tests/FreeX.App.Host.Tests/SymbolPickerDialogSourceTests.cs
+++ tests/FreeX.App.Host.Tests/SymbolPickerDialogSourceTests.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using System.IO;
-using System.Windows;
 
 namespace FreeX.App.Host.Tests;
 
@@ -11,17 +10,4 @@ public sealed partial class SymbolPickerDialogSourceTests
         File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "SymbolPickerDialog.Layout.cs")) +
         File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "SymbolPickerDialog.Catalog.cs")) +
         File.ReadAllText(WorkspaceFileLocator.Find("src", "FreeX.App.Host", "SymbolPickerSelectionPlanner.cs"));
-
-    private static IEnumerable<T> FindLogicalChildren<T>(DependencyObject parent)
-        where T : DependencyObject
-    {
-        foreach (var child in LogicalTreeHelper.GetChildren(parent).OfType<DependencyObject>())
-        {
-            if (child is T match)
-                yield return match;
-
-            foreach (var descendant in FindLogicalChildren<T>(child))
-                yield return descendant;
-        }
-    }
 }
