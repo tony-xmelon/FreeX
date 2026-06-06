66b7a2304467b7c4f591a7ad376eacfc6c5ade7e
tony-xmelon <tony.xmelon@gmail.com>
2026-06-06 04:57:08 +0300

    Validate comment patch saves

--- tests/FreeX.Core.IO.Tests/XlsxNonChartSchemaValidationTests.WorksheetFeatures.cs
+++ tests/FreeX.Core.IO.Tests/XlsxNonChartSchemaValidationTests.WorksheetFeatures.cs
@@ -116,6 +116,49 @@ public sealed partial class XlsxNonChartSchemaValidationTests
         SchemaErrors(workbook).Should().BeEmpty();
     }
 
+    [Fact]
+    public void LoadedWorkbookPatchSave_WithComments_ProducesSchemaValidWorkbook()
+    {
+        using var source = CreateLegacyCommentSourcePackage();
+        var sourceComments = ReadPackageRootElement(source, "xl/comments1.xml");
+        var sourceVmlDrawing = ReadPackageRootElement(source, "xl/drawings/vmlDrawing1.vml");
+        var sourceWorksheetRelationships = ReadPackageRootElement(source, "xl/worksheets/_rels/sheet1.xml.rels");
+        var sourceLegacyDrawing = ReadWorksheetChildElement(source, "legacyDrawing");
+        source.Position = 0;
+
+        var adapter = new XlsxFileAdapter();
+        var workbook = adapter.Load(source);
+        XlsxFileAdapter.TryPrepareLoadedPackageSnapshotForEdit(workbook, out var blockReason)
+            .Should()
+            .BeTrue(blockReason);
+
+        var sheet = workbook.GetSheetAt(0);
+        sheet.SetCell(new CellAddress(sheet.Id, 4, 2), new NumberValue(42));
+
+        using var saved = new MemoryStream();
+        adapter.Save(workbook, saved);
+
+        adapter.LastSaveDiagnostics.Path.Should().Be(XlsxSavePath.SourcePatch);
+        adapter.LastSaveDiagnostics.Reason.Should().Be("patch_applied");
+        SchemaErrors(saved).Should().BeEmpty();
+        ReadPackageRootElement(saved, "xl/comments1.xml")
+            .ToString(SaveOptions.DisableFormatting)
+            .Should()
+            .Be(sourceComments.ToString(SaveOptions.DisableFormatting));
+        ReadPackageRootElement(saved, "xl/drawings/vmlDrawing1.vml")
+            .ToString(SaveOptions.DisableFormatting)
+            .Should()
+            .Be(sourceVmlDrawing.ToString(SaveOptions.DisableFormatting));
+        ReadPackageRootElement(saved, "xl/worksheets/_rels/sheet1.xml.rels")
+            .ToString(SaveOptions.DisableFormatting)
+            .Should()
+            .Be(sourceWorksheetRelationships.ToString(SaveOptions.DisableFormatting));
+        ReadWorksheetChildElement(saved, "legacyDrawing")
+            .ToString(SaveOptions.DisableFormatting)
+            .Should()
+            .Be(sourceLegacyDrawing.ToString(SaveOptions.DisableFormatting));
+    }
+
 
     [Fact]
     public void FreezePanes_ProducesSchemaValidWorkbook()
@@ -226,6 +269,123 @@ public sealed partial class XlsxNonChartSchemaValidationTests
 
         return workbook;
     }
+
+    private static MemoryStream CreateLegacyCommentSourcePackage() =>
+        XlsxPackageTestFixtures.CreatePackage(
+            (
+                "[Content_Types].xml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+                  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+                  <Default Extension="xml" ContentType="application/xml"/>
+                  <Default Extension="vml" ContentType="application/vnd.openxmlformats-officedocument.vmlDrawing"/>
+                  <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
+                  <Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/>
+                  <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
+                  <Override PartName="/xl/comments1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.comments+xml"/>
+                </Types>
+                """),
+            (
+                "_rels/.rels",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
+                </Relationships>
+                """),
+            (
+                "xl/workbook.xml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+                  <sheets>
+                    <sheet name="Data" sheetId="1" r:id="rId1"/>
+                  </sheets>
+                </workbook>
+                """),
+            (
+                "xl/_rels/workbook.xml.rels",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
+                  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
+                </Relationships>
+                """),
+            (
+                "xl/styles.xml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+                  <fonts count="1"><font><sz val="11"/><color theme="1"/><name val="Calibri"/><family val="2"/><scheme val="minor"/></font></fonts>
+                  <fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill></fills>
+                  <borders count="1"><border><left/><right/><top/><bottom/><diagonal/></border></borders>
+                  <cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>
+                  <cellXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"/></cellXfs>
+                  <cellStyles count="1"><cellStyle name="Normal" xfId="0" builtinId="0"/></cellStyles>
+                  <dxfs count="0"/>
+                  <tableStyles count="0" defaultTableStyle="TableStyleMedium2" defaultPivotStyle="TableStyleLight16"/>
+                </styleSheet>
+                """),
+            (
+                "xl/worksheets/sheet1.xml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+                  <dimension ref="A1:C2"/>
+                  <sheetData>
+                    <row r="1"><c r="A1" t="inlineStr"><is><t>source</t></is></c></row>
+                    <row r="2"><c r="C2" t="inlineStr"><is><t>review</t></is></c></row>
+                  </sheetData>
+                  <legacyDrawing r:id="rId2"/>
+                </worksheet>
+                """),
+            (
+                "xl/worksheets/_rels/sheet1.xml.rels",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments" Target="../comments1.xml"/>
+                  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/vmlDrawing" Target="../drawings/vmlDrawing1.vml"/>
+                </Relationships>
+                """),
+            (
+                "xl/comments1.xml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <comments xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+                  <authors>
+                    <author>Excel Reviewer</author>
+                  </authors>
+                  <commentList>
+                    <comment ref="C2" authorId="0">
+                      <text><r><t>Original note</t></r></text>
+                    </comment>
+                  </commentList>
+                </comments>
+                """),
+            (
+                "xl/drawings/vmlDrawing1.vml",
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <xml xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel">
+                  <v:shape id="_x0000_s1025" type="#_x0000_t202" style="position:absolute;margin-left:80pt;margin-top:6pt;width:108pt;height:59.25pt;z-index:1;visibility:hidden" fillcolor="#ffffe1" o:insetmode="auto">
+                    <v:fill color2="#ffffe1"/>
+                    <v:shadow color="black" obscured="t"/>
+                    <v:path o:connecttype="none"/>
+                    <v:textbox style="mso-direction-alt:auto"><div style="text-align:left"/></v:textbox>
+                    <x:ClientData ObjectType="Note">
+                      <x:MoveWithCells/>
+                      <x:SizeWithCells/>
+                      <x:Anchor>2, 15, 1, 2, 4, 15, 5, 3</x:Anchor>
+                      <x:AutoFill>False</x:AutoFill>
+                      <x:Row>1</x:Row>
+                      <x:Column>2</x:Column>
+                    </x:ClientData>
+                  </v:shape>
+                </xml>
+                """));
 
     private static XElement ReadPackageRootElement(Stream stream, string entryName)
     {
