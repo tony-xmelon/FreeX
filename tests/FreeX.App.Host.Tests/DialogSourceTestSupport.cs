e0701893d9ee6881e73a0f13f9e07271d9c7023a
tony-xmelon <tony.xmelon@gmail.com>
2026-06-06 17:52:37 +0300

    Allow App Host XAML helper path segments

--- tests/FreeX.App.Host.Tests/DialogSourceTestSupport.cs
+++ tests/FreeX.App.Host.Tests/DialogSourceTestSupport.cs
@@ -19,8 +19,9 @@ internal static class DialogSourceTestSupport
     public static string ReadAppUiSourcesWithSeparator(string separator, params string[] fileNames) =>
         string.Join(separator, fileNames.Select(ReadAppUiSource));
 
-    public static XDocument LoadHostXamlDocument(string fileName) =>
-        XDocument.Load(WorkspaceFileLocator.Find("src", "FreeX.App.Host", fileName));
+    public static XDocument LoadHostXamlDocument(params string[] relativeParts) =>
+        XDocument.Load(WorkspaceFileLocator.Find(
+            new[] { "src", "FreeX.App.Host" }.Concat(relativeParts).ToArray()));
 
     public static string ReadClassSource(string fileName, string startMarker, string endMarker)
     {
