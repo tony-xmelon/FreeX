4cf120e574879feb47e15bd0d1688f44d042de71
tony-xmelon <tony.xmelon@gmail.com>
2026-06-06 20:12:10 +0300

    Add shared workspace read helpers

--- tests/FreeX.App.UI.Tests/WorkspaceFileLocator.cs
+++ tests/FreeX.App.UI.Tests/WorkspaceFileLocator.cs
@@ -7,4 +7,7 @@ internal static class WorkspaceFileLocator
 
     public static string FindWithFailureMessage(string message, params string[] relativeParts) =>
         TestWorkspaceFileLocator.FindWithFailureMessage(message, relativeParts);
+
+    public static string ReadAllTextWithFailureMessage(string message, params string[] relativeParts) =>
+        TestWorkspaceFileLocator.ReadAllTextWithFailureMessage(message, relativeParts);
 }
