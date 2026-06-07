27d4eae07468133b59ad06397527063d1f1f39bf
tony-xmelon <tony.xmelon@gmail.com>
2026-06-07 20:55:04 +0300

    Share page break normalization helpers

--- src/FreeX.Core.IO/XlsxWorksheetPageBreakNormalizer.cs
+++ src/FreeX.Core.IO/XlsxWorksheetPageBreakNormalizer.cs
@@ -13,7 +13,7 @@ internal static class XlsxWorksheetPageBreakNormalizer
     public static bool NormalizeElement(XElement pageBreaks)
     {
         var changed = false;
-        changed |= RemoveUnknownAttributes(pageBreaks, PageBreaksAttributes);
+        changed |= XlsxXmlNormalizationHelpers.RemoveUnknownAttributes(pageBreaks, PageBreaksAttributes);
         changed |= RemoveUnexpectedChildren(pageBreaks);
 
         foreach (var breakElement in pageBreaks.Elements(WorksheetNs + "brk").ToList())
@@ -24,8 +24,8 @@ internal static class XlsxWorksheetPageBreakNormalizer
             .Elements(WorksheetNs + "brk")
             .Count(element => !string.Equals(element.Attribute("man")?.Value, "0", StringComparison.Ordinal));
 
-        changed |= SetAttributeIfChanged(pageBreaks, "count", breakCount.ToString(CultureInfo.InvariantCulture));
-        changed |= SetAttributeIfChanged(pageBreaks, "manualBreakCount", manualBreakCount.ToString(CultureInfo.InvariantCulture));
+        changed |= XlsxXmlNormalizationHelpers.SetAttributeIfChanged(pageBreaks, "count", breakCount.ToString(CultureInfo.InvariantCulture));
+        changed |= XlsxXmlNormalizationHelpers.SetAttributeIfChanged(pageBreaks, "manualBreakCount", manualBreakCount.ToString(CultureInfo.InvariantCulture));
         return changed;
     }
 
@@ -63,32 +63,13 @@ internal static class XlsxWorksheetPageBreakNormalizer
         }
 
         var changed = false;
-        changed |= RemoveUnknownAttributes(breakElement, BreakAttributes);
-        changed |= SetAttributeIfChanged(breakElement, "id", normalizedId);
-        changed |= NormalizeAttribute(breakElement, "min", NormalizeUnsignedIntOrNull);
-        changed |= NormalizeAttribute(breakElement, "max", NormalizeUnsignedIntOrNull);
-        changed |= NormalizeAttribute(breakElement, "man", NormalizeBoolean);
-        changed |= NormalizeAttribute(breakElement, "pt", NormalizeBoolean);
+        changed |= XlsxXmlNormalizationHelpers.RemoveUnknownAttributes(breakElement, BreakAttributes);
+        changed |= XlsxXmlNormalizationHelpers.SetAttributeIfChanged(breakElement, "id", normalizedId);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(breakElement, "min", NormalizeUnsignedIntOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(breakElement, "max", NormalizeUnsignedIntOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(breakElement, "man", NormalizeBoolean);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(breakElement, "pt", NormalizeBoolean);
         return changed;
-    }
-
-    private static bool NormalizeAttribute(
-        XElement element,
-        string attributeName,
-        Func<string?, string?> normalize)
-    {
-        var attribute = element.Attribute(attributeName);
-        var normalized = normalize(attribute?.Value);
-        if (normalized is null)
-        {
-            if (attribute is null)
-                return false;
-
-            attribute.Remove();
-            return true;
-        }
-
-        return SetAttributeIfChanged(element, attributeName, normalized);
     }
 
     private static bool RemoveUnexpectedChildren(XElement pageBreaks)
@@ -104,34 +85,6 @@ internal static class XlsxWorksheetPageBreakNormalizer
         }
 
         return changed;
-    }
-
-    private static bool RemoveUnknownAttributes(XElement element, IReadOnlySet<string> allowedNames)
-    {
-        var changed = false;
-        foreach (var attribute in element.Attributes().ToList())
-        {
-            if (attribute.IsNamespaceDeclaration ||
-                (attribute.Name.NamespaceName.Length == 0 && allowedNames.Contains(attribute.Name.LocalName)))
-            {
-                continue;
-            }
-
-            attribute.Remove();
-            changed = true;
-        }
-
-        return changed;
-    }
-
-    private static bool SetAttributeIfChanged(XElement element, string attributeName, string value)
-    {
-        var attribute = element.Attribute(attributeName);
-        if (attribute is not null && string.Equals(attribute.Value, value, StringComparison.Ordinal))
-            return false;
-
-        element.SetAttributeValue(attributeName, value);
-        return true;
     }
 
     private static string? NormalizeBoolean(string? value)
