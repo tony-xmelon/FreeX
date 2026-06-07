eea30c1a3788ba99e10c64b4da7dcf8030703de3
tony-xmelon <tony.xmelon@gmail.com>
2026-06-07 04:01:29 +0300

    Share formula source test helper

--- tests/FreeX.Core.Formula.Tests/FormulaSourceTestSupport.cs
+++
@@ -1,7 +0,0 @@
-namespace FreeX.Core.Formula.Tests;
-
-internal static class FormulaSourceTestSupport
-{
-    public static string ReadFormulaSource(string fileName) =>
-        WorkspaceFileLocator.ReadAllText("src", "FreeX.Core.Formula", fileName);
-}
