12cc2e04a12cd2c3d473c1a4860e5b7dbe330265
tony-xmelon <tony.xmelon@gmail.com>
2026-06-07 18:58:21 +0300

    Share workbook core normalization helpers

--- src/FreeX.Core.IO/XlsxWorkbookProtectionNormalizer.cs
+++ src/FreeX.Core.IO/XlsxWorkbookProtectionNormalizer.cs
@@ -38,71 +38,21 @@ internal static class XlsxWorkbookProtectionNormalizer
     public static bool NormalizeElement(XElement workbookProtection)
     {
         var changed = false;
-        changed |= RemoveUnknownAttributes(workbookProtection);
-        changed |= RemoveAllNodes(workbookProtection);
+        changed |= XlsxXmlNormalizationHelpers.RemoveUnknownAttributes(workbookProtection, WorkbookProtectionAttributes);
+        changed |= XlsxXmlNormalizationHelpers.RemoveAllNodes(workbookProtection);
 
-        changed |= NormalizeAttribute(workbookProtection, "workbookPassword", NormalizeLegacyPasswordHashOrNull);
-        changed |= NormalizeAttribute(workbookProtection, "revisionsPassword", NormalizeLegacyPasswordHashOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "workbookPassword", NormalizeLegacyPasswordHashOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "revisionsPassword", NormalizeLegacyPasswordHashOrNull);
         foreach (var attributeName in BooleanAttributes)
-            changed |= NormalizeAttribute(workbookProtection, attributeName, NormalizeBoolean);
+            changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, attributeName, NormalizeBoolean);
         foreach (var attributeName in UnsignedIntAttributes)
-            changed |= NormalizeAttribute(workbookProtection, attributeName, NormalizeUnsignedIntOrNull);
-        changed |= NormalizeAttribute(workbookProtection, "revisionsHashValue", NormalizeBase64BinaryOrNull);
-        changed |= NormalizeAttribute(workbookProtection, "revisionsSaltValue", NormalizeBase64BinaryOrNull);
-        changed |= NormalizeAttribute(workbookProtection, "workbookHashValue", NormalizeBase64BinaryOrNull);
-        changed |= NormalizeAttribute(workbookProtection, "workbookSaltValue", NormalizeBase64BinaryOrNull);
+            changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, attributeName, NormalizeUnsignedIntOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "revisionsHashValue", NormalizeBase64BinaryOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "revisionsSaltValue", NormalizeBase64BinaryOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "workbookHashValue", NormalizeBase64BinaryOrNull);
+        changed |= XlsxXmlNormalizationHelpers.NormalizeAttribute(workbookProtection, "workbookSaltValue", NormalizeBase64BinaryOrNull);
 
         return changed;
-    }
-
-    private static bool RemoveUnknownAttributes(XElement element)
-    {
-        var changed = false;
-        foreach (var attribute in element.Attributes().ToList())
-        {
-            if (attribute.IsNamespaceDeclaration ||
-                (attribute.Name.NamespaceName.Length == 0 && WorkbookProtectionAttributes.Contains(attribute.Name.LocalName)))
-            {
-                continue;
-            }
-
-            attribute.Remove();
-            changed = true;
-        }
-
-        return changed;
-    }
-
-    private static bool RemoveAllNodes(XElement element)
-    {
-        if (!element.Nodes().Any())
-            return false;
-
-        element.RemoveNodes();
-        return true;
-    }
-
-    private static bool NormalizeAttribute(
-        XElement element,
-        string attributeName,
-        Func<string?, string?> normalize)
-    {
-        var attribute = element.Attribute(attributeName);
-        var normalized = normalize(attribute?.Value);
-        if (normalized is null)
-        {
-            if (attribute is null)
-                return false;
-
-            attribute.Remove();
-            return true;
-        }
-
-        if (attribute is not null && string.Equals(attribute.Value, normalized, StringComparison.Ordinal))
-            return false;
-
-        element.SetAttributeValue(attributeName, normalized);
-        return true;
     }
 
     private static string? NormalizeBoolean(string? value)
