4edf7c5be54de9e84bb5fd6a0f3a71d8245b5c96
tony-xmelon <tony.xmelon@gmail.com>
2026-06-08 18:16:08 +0300

    Refactor worksheet IO lookup helpers

--- src/FreeX.Core.IO/XlsxWorksheetDiagnosticsMapper.cs
+++ src/FreeX.Core.IO/XlsxWorksheetDiagnosticsMapper.cs
@@ -104,10 +104,17 @@ internal static partial class XlsxWorksheetDiagnosticsMapper
             ]
         };
 
-        var insertionPoint = worksheetRoot.Elements()
-            .FirstOrDefault(element =>
-                element.Name.Namespace == workbookNs &&
-                laterWorksheetElements.Contains(element.Name.LocalName, StringComparer.Ordinal));
+        XElement? insertionPoint = null;
+        foreach (var element in worksheetRoot.Elements())
+        {
+            if (element.Name.Namespace == workbookNs &&
+                laterWorksheetElements.Contains(element.Name.LocalName, StringComparer.Ordinal))
+            {
+                insertionPoint = element;
+                break;
+            }
+        }
+
         if (insertionPoint is null)
             worksheetRoot.Add(metadataElement);
         else
