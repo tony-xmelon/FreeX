274500ad8a1bb4c851ec4d9af1c72b739a86ea1d
tony-xmelon <tony.xmelon@gmail.com>
2026-06-11 02:17:34 +0300

    Fix Custom Sort dialog layout

--- tests/FreeX.App.Host.Tests/SortDialogTests.DialogInteraction.cs
+++ tests/FreeX.App.Host.Tests/SortDialogTests.DialogInteraction.cs
@@ -8,6 +8,63 @@ namespace FreeX.App.Host.Tests;
 public sealed partial class SortDialogTests
 {
     [Fact]
+    public void DialogLayout_KeepsSortLevelToolbarAndActionButtonsSeparated()
+    {
+        StaTestRunner.Run(() =>
+        {
+            var dialog = new SortDialog([
+                new SortDialogLevel(0, true),
+                new SortDialogLevel(1, false),
+                new SortDialogLevel(2, true)
+            ]);
+            dialog.Width = dialog.MinWidth;
+            dialog.Height = dialog.MinHeight;
+            dialog.Show();
+
+            try
+            {
+                dialog.UpdateLayout();
+
+                dialog.ResizeMode.Should().Be(ResizeMode.CanResizeWithGrip);
+                var root = dialog.Content.Should().BeOfType<Grid>().Subject;
+                root.RowDefinitions.Should().HaveCount(4);
+                root.RowDefinitions[1].Height.GridUnitType.Should().Be(GridUnitType.Star);
+
+                var levelsGrid = GetControl<DataGrid>(dialog, "_levelsGrid");
+                var add = GetControl<Button>(dialog, "_addLevelButton");
+                var delete = GetControl<Button>(dialog, "_deleteLevelButton");
+                var copy = GetControl<Button>(dialog, "_copyLevelButton");
+                var moveUp = GetControl<Button>(dialog, "_moveUpButton");
+                var moveDown = GetControl<Button>(dialog, "_moveDownButton");
+                var options = GetControl<Button>(dialog, "_optionsButton");
+                var ok = WpfTestTree.FindVisualDescendants<Button>(dialog).Single(button => button.IsDefault);
+                var cancel = WpfTestTree.FindVisualDescendants<Button>(dialog).Single(button => button.IsCancel);
+                var toolbarButtons = new[] { add, delete, copy, moveUp, moveDown, options };
+
+                levelsGrid.ActualHeight.Should().BeGreaterThan(180);
+                var gridBounds = BoundsRelativeTo(root, levelsGrid);
+                toolbarButtons.Min(button => BoundsRelativeTo(root, button).Top)
+                    .Should()
+                    .BeGreaterThanOrEqualTo(gridBounds.Bottom - 0.5);
+                var toolbarBottom = toolbarButtons.Max(button => BoundsRelativeTo(root, button).Bottom);
+                BoundsRelativeTo(root, ok).Top.Should().BeGreaterThanOrEqualTo(toolbarBottom - 0.5);
+                BoundsRelativeTo(root, cancel).Top.Should().BeGreaterThanOrEqualTo(toolbarBottom - 0.5);
+
+                foreach (var element in toolbarButtons.Cast<FrameworkElement>().Append(ok).Append(cancel).Prepend(levelsGrid))
+                {
+                    element.ActualWidth.Should().BeGreaterThan(0);
+                    element.ActualHeight.Should().BeGreaterThan(0);
+                    AssertInside(root, element);
+                }
+            }
+            finally
+            {
+                dialog.Close();
+            }
+        });
+    }
+
+    [Fact]
     public void ToolbarButtons_EnableOnlyValidLevelActions()
     {
         StaTestRunner.Run(() =>
