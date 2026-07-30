b34f8ab7bea746b5ba2dd3fee77bbdc9790c7853
Antoni Ivanov <tony.xmelon@gmail.com>
2026-07-30 08:51:56 +0300

    FreeP: keep Picture Strips SmartArt live

--- freep/FreeP.App.Presentation/SmartArtLayoutEngine.cs
+++ freep/FreeP.App.Presentation/SmartArtLayoutEngine.cs
@@ -3053,7 +3053,7 @@ public static class SmartArtLayoutEngine
             return false;
 
         var id = uniqueId.Replace('\\', '/').Trim().ToLowerInvariant();
-        return id.Split('/').Last() is "picturelineup" or "continuouspicturelist";
+        return id.Split('/').Last() is "picturelineup" or "picturestrips" or "continuouspicturelist";
     }
 
     private static bool IsPictureGridLayout(string uniqueId)
