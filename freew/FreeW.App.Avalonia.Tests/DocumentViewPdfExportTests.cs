422a80df8908cd06d4081a87213a695beb6f1654
Antoni Ivanov <tony.xmelon@gmail.com>
2026-08-02 18:01:20 +0300

    Thin Painted Eggs right shell

--- freew/FreeW.App.Avalonia.Tests/DocumentViewPdfExportTests.cs
+++ freew/FreeW.App.Avalonia.Tests/DocumentViewPdfExportTests.cs
@@ -1526,7 +1526,7 @@ public sealed class DocumentViewPdfExportTests
                 if (pixel.Red > 245 && pixel.Green > 245 && pixel.Blue > 245)
                     whiteEggInterior++;
             }
-            blackInk.Should().BeInRange(250, 260);
+            blackInk.Should().BeInRange(228, 238);
             whiteEggInterior.Should().BeGreaterThan(200);
             bitmap.GetPixel(408, 528).Should().Be(SKColors.White);
         }, CancellationToken.None);
