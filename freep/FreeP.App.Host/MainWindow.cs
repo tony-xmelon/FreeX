fcfa801d9c07f64e2906823ed5e4c8cdcbac4e1b
Antoni Ivanov <tony.xmelon@gmail.com>
2026-08-04 03:31:10 +0300

    FreeP: expose chart data-label context targets

--- freep/FreeP.App.Host/MainWindow.cs
+++ freep/FreeP.App.Host/MainWindow.cs
@@ -733,6 +733,10 @@ public sealed partial class MainWindow : Window
                 Add("Format Data Point...", () => OpenChartPointOptionsDialog(hit.SeriesIndex, hit.PointIndex));
                 Add("Format Data Series...", () => OpenChartSeriesOptionsDialog(hit.SeriesIndex));
                 break;
+            case ChartSubtargetKind.DataLabel:
+                Add("Format Data Label...", () => OpenChartPointOptionsDialog(hit.SeriesIndex, hit.PointIndex));
+                Add("Format Data Series...", () => OpenChartSeriesOptionsDialog(hit.SeriesIndex));
+                break;
             case ChartSubtargetKind.Series:
                 Add("Format Data Series...", () => OpenChartSeriesOptionsDialog(hit.SeriesIndex));
                 break;
