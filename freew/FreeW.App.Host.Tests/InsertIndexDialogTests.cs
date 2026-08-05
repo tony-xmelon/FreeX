fef6392ab7b67e28abb87fb79321ef52c0026897
Antoni Ivanov <tony.xmelon@gmail.com>
2026-08-05 23:36:28 +0300

    Add WPF identifier-aware index updates

--- freew/FreeW.App.Host.Tests/InsertIndexDialogTests.cs
+++ freew/FreeW.App.Host.Tests/InsertIndexDialogTests.cs
@@ -1,4 +1,5 @@
 using FreeW.App.Host;
+using FreeW.App.Presentation.Ribbon;
 
 namespace FreeW.App.Host.Tests;
 
@@ -21,6 +22,18 @@ public sealed class InsertIndexDialogTests
         var dialog = InsertIndexDialog.CreateForTest();
         dialog.SetIdentifierForTest(" People ");
 
+        dialog.AcceptForTest();
+
+        dialog.ResultForTest.Should().BeEquivalentTo(new { Identifier = "People" });
+    }
+
+    [StaFact]
+    public void UpdateDialog_UsesUpdateChromeAndTrimsOptionalIdentifier()
+    {
+        var dialog = InsertIndexDialog.CreateForUpdateTest(" People ");
+
+        dialog.Title.Should().Be(InsertIndexDialogPlanner.UpdateTitle);
+        dialog.ActionLabelForTest.Should().Be(InsertIndexDialogPlanner.UpdateButtonLabel);
         dialog.AcceptForTest();
 
         dialog.ResultForTest.Should().BeEquivalentTo(new { Identifier = "People" });
